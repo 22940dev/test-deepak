8bcae571cb81f915acbaae5cf4441095c70a7898
Deepak <deepakrajan.grea@gmail.com>
2021-04-10 21:19:05 +0530

    header start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+
+
+
 function App() {
   return (
     <div className="App">
-      <div className="child">
-      <h3>The site is under construction. </h3>
-      </div>
+      <Header/>
     </div>
   );
 }
