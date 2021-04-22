d31fc511e9a022ac5184d675632a87c41425a7eb
Deepak <deepakrajan.grea@gmail.com>
2021-04-22 13:54:54 +0530

    archeieve landing page

--- src/components/archieve/Archieves.js
+++ src/components/archieve/Archieves.js
@@ -1,10 +1,103 @@
-import "./Archieve.css";
+import styled from "styled-components";
+import {MidWrapper, CatHeading} from "../../elements/Layout";
+import "./Archieves.css";
 
+
+const ArchieveCard = styled.div`
+
+`
 
 function Archieves(){
     return (
         <>
-        All archieves
+        <MidWrapper>
+            <CatHeading>
+                Welcome to My <b>Archieves</b>
+            </CatHeading>
+            <div className="archieve-card">
+                <div className="archieve-heading">
+                       Creating a Tool for <b>Maya Project Automation </b>
+                    <div className="archieve-date">
+                        20 September 2020
+                    </div>
+                </div>
+                <div></div>
+                <img src="https://cdna.artstation.com/p/assets/images/images/036/927/388/large/danar-worya-boss-zoomin-4k.jpg?1619008414"></img>
+
+                <div className="archieve-content">
+                    <p>
+                Wolfwalkers was the first Cartoon Saloon movie with a proper story team.
+                 Prior to it, there had been story artists on their films, but never as a
+                  deliberate effort to mimic the system commonly used in Hollywood.
+                   That means having a group of people translating sequences from script to animatic 
+                   largely on their own and pitching them to the directors. Read More
+                    </p>
+                </div>
+            </div> 
+            <div className="archieve-card">
+                <div></div>
+                <div className="archieve-heading">
+                        Making of <b>Cassowary </b>using Ziva Dynamics.
+                    <div className="archieve-date">
+                        20 September 2020
+                    </div>
+                </div>
+                <div className="archieve-content">
+                    <p>
+                Wolfwalkers was the first Cartoon Saloon movie with a proper story team.
+                 Prior to it, there had been story artists on their films, but never as a
+                  deliberate effort to mimic the system commonly used in Hollywood.
+                   That means having a group of people translating sequences from script to animatic 
+                   largely on their own and pitching them to the directors. Read More
+                    </p>
+                </div>
+                <img src="https://cdna.artstation.com/p/assets/images/images/036/937/614/4k/alex-alvarez-vicar-3k.jpg?1619027638"></img>
+
+            </div> 
+
+            <div className="archieve-card">
+                <div className="archieve-heading">
+                        Making of <b>Cassowary </b>using Ziva Dynamics.
+                    <div className="archieve-date">
+                        20 September 2020
+                    </div>
+                </div>
+                <div></div>
+                <img src="https://cdna.artstation.com/p/assets/images/images/036/927/388/large/danar-worya-boss-zoomin-4k.jpg?1619008414"></img>
+
+                <div className="archieve-content">
+                    <p>
+                Wolfwalkers was the first Cartoon Saloon movie with a proper story team.
+                 Prior to it, there had been story artists on their films, but never as a
+                  deliberate effort to mimic the system commonly used in Hollywood.
+                   That means having a group of people translating sequences from script to animatic 
+                   largely on their own and pitching them to the directors. Read More
+                    </p>
+                </div>
+            </div> 
+
+
+        <div className="archieve-card">
+                <div></div>
+                <div className="archieve-heading">
+                        Making of <b>Cassowary </b>using Ziva Dynamics.
+                    <div className="archieve-date">
+                        20 September 2020
+                    </div>
+                </div>
+                <div className="archieve-content">
+                    <p>
+                Wolfwalkers was the first Cartoon Saloon movie with a proper story team.
+                 Prior to it, there had been story artists on their films, but never as a
+                  deliberate effort to mimic the system commonly used in Hollywood.
+                   That means having a group of people translating sequences from script to animatic 
+                   largely on their own and pitching them to the directors. Read More
+                    </p>
+                </div>
+                <img src="https://cdna.artstation.com/p/assets/images/images/036/937/614/4k/alex-alvarez-vicar-3k.jpg?1619027638"></img>
+
+            </div> 
+        </MidWrapper>
         </>
     )
 }
